fc514bcef3fee78eaf1be2037c5ea2d3054c7298
长鞭星 <120235826+ScourgeStar@users.noreply.github.com>
2025-04-04 18:37:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,15 @@
 #SBATCH -e slurm-error/winograd-job-%j.err
 #SBATCH -c 64
 #SBATCH --exclusive
-#SBATCH --exclude hepnode0
+#SBATCH --exclude hepnode2
 
 # Note: How to run this script on slurm: `sbatch ./run.sh'.
 # Note: see `man sbatch' for more options.
 
 # Note: Manual control number of omp threads
-# export OMP_NUN_THREADS=64
+# export OMP_NUN_THREADS=60
 
 # Note: numactl - Control NUMA policy for processes or shared memory, see `man numactl'.`
 # Note: perf-stat - Run a command and gather performance counter statistics, see `man perf stat'.
 
-numactl --cpunodebind=0-3 --membind=0-3 perf stat -ddd ./winograd conf/small.conf 
+numactl --cpunodebind=0-3 --membind=0-3 perf stat -ddd ./winograd conf/vgg16.conf 
